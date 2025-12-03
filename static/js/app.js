dd1b12c861151cf686f1304835744c62233ffde8
lukas <contact@lukaswalker.ch>
2025-12-03 18:20:48 +0100

    testing 20 fps

--- static/js/app.js
+++ static/js/app.js
@@ -50,7 +50,7 @@ btn.addEventListener('click', () => {
     // start polling snapshots
     videoTimer = setInterval(() => {
       img.src = '/api/snapshot?ts=' + Date.now();
-    }, 300);
+    }, 50);
   } else {
     container.style.display = 'none';
     img.src = '';
